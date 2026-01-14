abb5f454a1f74f844844a1d7e70715c6c7f76459
Rajibul <rajibulislam62963@gmail.com>
2026-01-14 21:26:08 +0530

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const connectDB = require('./server/config/db');
 const { isActiveRoute } = require('./server/helpers/routeHelpers');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5005;
   
 // Connect to DB
 connectDB();
